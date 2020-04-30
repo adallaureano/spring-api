b3a539ebd2847366f145e0e46aa28fb06111a5d2
adallauriano <laureano.adalberto@gmail.com>
2020-04-30 13:19:23 +0100

    new project spring api with conection to a postgres database using docker.

---
+++ src/main/java/devops/datasource/PostgresDatasource.java
@@ -0,0 +1,22 @@
+package devops.datasource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PostgresDatasource {
+
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource hikariDataSource(){
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+
+}
